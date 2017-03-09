310fc15d94784054f1bb09ef1d760688e92972ee
Bochkarev Maxim <qontrol1488@gmail.com>
2017-03-09 11:36:13 +0300

    Added HttpLogHandler class

---
+++ SecureData/SecureData/HttpLogHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureData
+{
+    public class HttpLogHandler
+    {
+        protected HttpResult _currentLog;
+        protected IHttpResult _handler;
+
+        public HttpLogHandler(IHttpResult handler)
+        {
+            _handler = handler;
+        }
+
+        public HttpResult GetCurrentLog()
+        {
+            return _currentLog;
+        }
+        
+        public void Process(HttpResult httpResult)
+        {
+            var securedHttpResult = new HttpResult();
+            securedHttpResult = _handler.GetSecuredResult(httpResult);
+            Log(securedHttpResult);
+        }
+
+        public void Process(string url, string req, string res)
+        {
+            var httpResult = new HttpResult
+            {
+                Url = url,
+                RequestBody = req,
+                ResponseBody = res
+            };
+
+            Process(httpResult);
+        }
+
+        protected void Log(HttpResult result)
+        {
+            _currentLog = new HttpResult
+            {
+                Url = result.Url,
+                RequestBody = result.RequestBody,
+                ResponseBody = result.ResponseBody
+            };
+        }
+    }
+}
