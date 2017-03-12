6489f97ed4c96871f532fbf3a86765090eef4413
Bochkarev Maxim <qontrol1488@gmail.com>
2017-03-12 23:14:13 +0300

    Added XmlDataHandler class and tests

---
+++ SecureData/SecureData/DataHandlers/XmlDataHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace SecureData.DataHandlers
+{
+    public class XmlDataHandler : AbstractDataHandler
+    {
+        public override string GetSecuredData(string data)
+        {
+            var xmlAttrHandler = new XmlAttributeDataHandler {Properties = Properties};
+            var xmlElemValueHandler = new XmlElementValueDataHandler {Properties = Properties};
+
+            return xmlAttrHandler.GetSecuredData(xmlElemValueHandler.GetSecuredData(data));
+        }
+    }
+}
---
+++ SecureData/SecureDataTests/DataHandlers/XmlDataHandlerTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecureData.DataHandlers;
+
+namespace SecureDataTests.DataHandlers
+{
+    [TestClass()]
+    public class XmlDataHandlerTests
+    {
+        [TestMethod()]
+        public void XmlDataHandler_GetSecuredData_EmptySecuredProps_NonChangedResult()
+        {
+            //ARRANGE
+            AbstractDataHandler handler = new XmlDataHandler();
+            handler.Properties = new string[] { };
+
+            //ACT
+            var result = handler.GetSecuredData("<auth user=\"max\"><pass>123456</pass></auth>");
+
+            //ASSERT
+            Assert.AreEqual("<auth user=\"max\"><pass>123456</pass></auth>", result);
+        }
+
+        [TestMethod()]
+        public void XmlDataHandler_GetSecuredData_NonEmptySecuredProps_ParametresSecured()
+        {
+            //ARRANGE
+            AbstractDataHandler handler = new XmlDataHandler();
+            handler.Properties = new string[] { "user", "pass" };
+
+            //ACT
+            var result = handler.GetSecuredData("<auth user=\"max\"><pass>123456</pass></auth>");
+
+            //ASSERT
+            Assert.AreEqual("<auth user=\"XXX\"><pass>XXXXXX</pass></auth>", result);
+        }
+
+
+        [TestMethod()]
+        public void XmlDataHandler_GetSecuredData_SecuredPropsNotInRequest_NonChangedResult()
+        {
+            //ARRANGE
+            AbstractDataHandler handler = new XmlDataHandler();
+            handler.Properties = new string[] { "first_name", "second_name" };
+
+            //ACT
+            var result = handler.GetSecuredData("<auth user=\"max\"><pass>123456</pass></auth>");
+
+            //ASSERT
+            Assert.AreEqual("<auth user=\"max\"><pass>123456</pass></auth>", result);
+        }
+
+        [TestMethod()]
+        public void XmlDataHandler_GetSecuredData_SomePropsNotInRequest_ListedParametresSecured()
+        {
+            //ARRANGE
+            AbstractDataHandler handler = new XmlDataHandler();
+            handler.Properties = new string[] { "user", "first_name" };
+
+            //ACT
+            var result = handler.GetSecuredData("<auth user=\"max\"><pass>123456</pass></auth>");
+
+            //ASSERT
+            Assert.AreEqual("<auth user=\"XXX\"><pass>123456</pass></auth>", result);
+        }
+    }
+}
