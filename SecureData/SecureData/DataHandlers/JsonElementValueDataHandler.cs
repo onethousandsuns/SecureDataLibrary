214e71526e608a1aadadc9ba4c835d320386746c
Bochkarev Maxim <qontrol1488@gmail.com>
2017-03-12 19:52:09 +0300

    Added JsonElementValue class and tests

---
+++ SecureData/SecureData/DataHandlers/JsonElementValueDataHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Script.Serialization;
+
+namespace SecureData.DataHandlers
+{
+    public class JsonElementValueDataHandler : AbstractDataHandler
+    {
+        public override string GetSecuredData(string data)
+        {
+            var serializer = new JavaScriptSerializer();
+            //dynamic dataDict = serializer.DeserializeObject(data);
+            var dataDict = serializer.Deserialize<dynamic>(data);
+
+            string str = null;
+            foreach (var key in Properties)
+            {
+                if (dataDict.ContainsKey(key))
+                {
+                    var x = dataDict[key]["value"];
+                    var securedValue = new string('X', dataDict[key]["value"].Length);
+                    dataDict[key]["value"] = securedValue;
+                }
+            }
+            var result = serializer.Serialize(dataDict);
+
+            return result;
+        }
+    }
+}
---
+++ SecureData/SecureDataTests/DataHandlers/JsonElementValueDataHandlerTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecureData.DataHandlers;
+
+namespace SecureDataTests.DataHandlers
+{
+    [TestClass()]
+    public class JsonElementValueDataHandlerTests
+    {
+        [TestMethod()]
+        public void JsonElementValueDataHandler_GetSecuredData_EmptySecuredProps_NonChangedResult()
+        {
+            //ARRANGE
+            AbstractDataHandler handler = new JsonElementValueDataHandler();
+            handler.Properties = new string[] {};
+
+            //ACT
+            var result = handler.GetSecuredData("{user:{value:\"max\"}, pass: {value:\"123456\"}}");
+
+            //ASSERT
+            Assert.AreEqual("{\"user\":{\"value\":\"max\"},\"pass\":{\"value\":\"123456\"}}", result);
+        }
+        
+        [TestMethod()]
+        public void JsonElementValueDataHandler_GetSecuredData_NonEmptySecuredProps_ParametresSecured()
+        {
+            //ARRANGE
+            AbstractDataHandler handler = new JsonElementValueDataHandler();
+            handler.Properties = new string[] { "user", "pass" };
+
+            //ACT
+            var result = handler.GetSecuredData("{user:{value:\"max\"}, pass: {value:\"123456\"}}");
+
+            //ASSERT
+            Assert.AreEqual("{\"user\":{\"value\":\"XXX\"},\"pass\":{\"value\":\"XXXXXX\"}}", result);
+        }
+
+        [TestMethod()]
+        public void JsonElementValueDataHandler_GetSecuredData_SecuredPropsNotInRequest_NonChangedResult()
+        {
+            //ARRANGE
+            AbstractDataHandler handler = new JsonElementValueDataHandler();
+            handler.Properties = new string[] { "username", "password" };
+
+            //ACT
+            var result = handler.GetSecuredData("{user:{value:\"max\"}, pass: {value:\"123456\"}}");
+
+            //ASSERT
+            Assert.AreEqual("{\"user\":{\"value\":\"max\"},\"pass\":{\"value\":\"123456\"}}", result);
+        }
+
+        [TestMethod()]
+        public void JsonElementValueDataHandler_GetSecuredData_SomePropsNotInRequest_ListedParametresSecured()
+        {
+            //ARRANGE
+            AbstractDataHandler handler = new JsonElementValueDataHandler();
+            handler.Properties = new string[] { "user", "first_name" };
+
+            //ACT
+            var result = handler.GetSecuredData("{user:{value:\"max\"}, pass: {value:\"123456\"}}");
+
+            //ASSERT
+            Assert.AreEqual("{\"user\":{\"value\":\"XXX\"},\"pass\":{\"value\":\"123456\"}}", result);
+        }
+    }
+}
