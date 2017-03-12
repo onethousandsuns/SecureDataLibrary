68ac735b4eca11848050cb79c33cbdc383257747
Bochkarev Maxim <qontrol1488@gmail.com>
2017-03-12 23:56:48 +0300

    Added RegexDataHandlerClass and tests

---
+++ SecureData/SecureData/DataHandlers/RegexDataHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SecureData.DataHandlers
+{
+    public class RegexDataHandler: AbstractDataHandler
+    {
+        private string RegExpString { get; set; }
+
+        public override string GetSecuredData(string data)
+        {
+            var result = data;
+
+            foreach (var key in Properties)
+            {
+                var replacedValueString = RegExpString.Replace("|REPLACED_VALUE|", key);
+                var elemRegex = new Regex(replacedValueString);
+                result = elemRegex.Replace(result, new string('X', elemRegex.Match(result).Length));
+            }
+            return result;
+        }
+
+        public RegexDataHandler(string regExp, string[] properties)
+        {
+            Properties = properties;
+            RegExpString = regExp;
+        }
+    }
+}
---
+++ SecureData/SecureDataTests/DataHandlers/RegexDataHandlerTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecureData.DataHandlers;
+
+namespace SecureDataTests.DataHandlers
+{
+    [TestClass()]
+    public class RegexDataHandlerTests
+    {
+        [TestMethod()]
+        public void RegexDataHandler_GetSecuredData_EmptySecuredProps_NonChangedResult()
+        {
+            //ARRANGE
+            string[] props = new string[] { };
+            string regExpStr = "(?<=\\(|REPLACED_VALUE|\\|).+?(?=\\||REPLACED_VALUE|\\))";
+
+            AbstractDataHandler handler = new RegexDataHandler(regExpStr, props);
+
+            //ACT
+            var result = handler.GetSecuredData("(user|max|user)(pass|max|pass)(lang|RU|lang)");
+
+            //ASSERT
+            Assert.AreEqual("(user|max|user)(pass|max|pass)(lang|RU|lang)", result);
+        }
+
+        [TestMethod()]
+        public void RegexDataHandler_GetSecuredData_NonEmptySecuredProps_ParametresSecured()
+        {
+            //ARRANGE
+            string[] props = new string[] { "user", "pass", "lang" };
+            string regExpStr = "(?<=\\(|REPLACED_VALUE|\\|).+?(?=\\||REPLACED_VALUE|\\))";
+
+            AbstractDataHandler handler = new RegexDataHandler(regExpStr, props);
+
+            //ACT
+            var result = handler.GetSecuredData("(user|max|user)(pass|max|pass)(lang|RU|lang)");
+
+            //ASSERT
+            Assert.AreEqual("(user|XXX|user)(pass|XXX|pass)(lang|XX|lang)", result);
+        }
+
+
+        [TestMethod()]
+        public void RegexDataHandler_GetSecuredData_SecuredPropsNotInRequest_NonChangedResult()
+        {
+            //ARRANGE
+            string[] props = new[] { "username", "password", "language" };
+            string regExpStr = "(?<=\\(|REPLACED_VALUE|\\|).+?(?=\\||REPLACED_VALUE|\\))";
+
+            AbstractDataHandler handler = new RegexDataHandler(regExpStr, props);
+
+            //ACT
+            var result = handler.GetSecuredData("(user|max|user)(pass|max|pass)(lang|RU|lang)");
+
+            //ASSERT
+            Assert.AreEqual("(user|max|user)(pass|max|pass)(lang|RU|lang)", result);
+        }
+
+        [TestMethod()]
+        public void RegexDataHandler_GetSecuredData_SomePropsNotInRequest_ListedParametresSecured()
+        {
+            //ARRANGE
+            string[] props = new[] { "user", "password", "lang" };
+            string regExpStr = "(?<=\\(|REPLACED_VALUE|\\|).+?(?=\\||REPLACED_VALUE|\\))";
+
+            AbstractDataHandler handler = new RegexDataHandler(regExpStr, props);
+
+            //ACT
+            var result = handler.GetSecuredData("(user|max|user)(pass|max|pass)(lang|RU|lang)");
+
+            //ASSERT
+            Assert.AreEqual("(user|XXX|user)(pass|max|pass)(lang|XX|lang)", result);
+        }
+    }
+}
