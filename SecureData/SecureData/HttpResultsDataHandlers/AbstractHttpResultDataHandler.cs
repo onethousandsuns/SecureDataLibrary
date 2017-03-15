dac2e22f2415ab7e322bea51c20668180e7b26a3
Bochkarev Maxim <qontrol1488@gmail.com>
2017-03-15 13:39:19 +0300

    Changed AbstractHttpResultDataHandler class

--- SecureData/SecureData/HttpResultsDataHandlers/AbstractHttpResultDataHandler.cs
+++ SecureData/SecureData/HttpResultsDataHandlers/AbstractHttpResultDataHandler.cs
@@ -1,16 +1,18 @@
-﻿using SecureData.DataHandlers;
+﻿using System.Collections.Generic;
+using System.Linq;
+using SecureData.DataHandlers;
 
 namespace SecureData
 {
+    using configParam = Dictionary<string, string>;
+
     public abstract class AbstractHttpResultDataHandler
     {
-        protected AbstractDataHandler UrlHandler { set; get; }
-        protected AbstractDataHandler RequestBodyHandler { set; get; }
-        protected AbstractDataHandler ResponseBodyHandler { set; get; }
+        protected Dictionary<string, configParam> Properties { get; set; }
 
         protected AbstractHttpResultDataHandler()
         {
-            InitHandlers();
+            Properties = new Dictionary<string, configParam>();
             InitProperties();
         }
 
@@ -18,21 +20,70 @@ namespace SecureData
         {
             HttpResult result = new HttpResult
             {
-                Url = UrlHandler.GetSecuredData(httpRes.Url),
-                ResponseBody = ResponseBodyHandler.GetSecuredData(httpRes.ResponseBody),
-                RequestBody = RequestBodyHandler.GetSecuredData(httpRes.RequestBody)
+                Url = GetHandledValue("URL", httpRes.Url),
+                ResponseBody = GetHandledValue("RES", httpRes.ResponseBody),
+                RequestBody = GetHandledValue("REQ", httpRes.RequestBody)
             };
+
             return result;
         }
-
-        protected void InitProperties(string[] securedProps)
+        protected string GetHandledValue(string paramType, string data)
         {
-            UrlHandler.Properties = securedProps;
-            RequestBodyHandler.Properties = securedProps;
-            ResponseBodyHandler.Properties = securedProps;
+            var handlerTypes = GetHandlerTypes(paramType);
+            var result = data;
+            foreach (var type in handlerTypes)
+            {
+                var handler = GetHandlerByTag(type);
+                handler.Properties = GetPropValueArrayByTag(paramType, type);
+                result = handler.GetSecuredData(result);
+            }
+            return result;
+        }
+        
+        protected string[] GetPropValueArrayByTag(string paramType, string handlerTag)
+        {
+            List<string> result = new List<string>();
+            foreach (var elem in Properties[paramType])
+            {
+                if (elem.Value == handlerTag)
+                {
+                    result.Add(elem.Key);
+                }
+            }
+            return result.ToArray();
         }
 
-        protected abstract void InitProperties();
-        protected abstract void InitHandlers();
+        protected List<string> GetHandlerTypes(string key)
+        {
+            return Properties[key].Values.ToList().Distinct().ToList();
+        }
+
+        protected AbstractDataHandler GetHandlerByTag(string tag)
+        {
+            switch (tag)
+            {
+                case "JSON_DATA":
+                    return new JsonDataHandler();
+                case "JSON_ELEM":
+                    return new JsonElementValueDataHandler();
+                    return new RestUrlDataHandler();
+                case "URL_GET":
+                    return new UrlGetRequestDataHandler();
+                case "XML_ATTR":
+                    return new XmlAttributeDataHandler();
+                case "XML_ELEM":
+                    return new XmlElementValueDataHandler();
+            }
+            return null;
+        }
+
+        protected void InitProperties(configParam urlProps, configParam requestProps, configParam responseProps)
+        {
+            Properties.Add("URL", urlProps);
+            Properties.Add("REQ", requestProps);
+            Properties.Add("RES", responseProps);
+        }
+
+        protected virtual void InitProperties(){ }
     }
 }
