f42ef29e0d7bf073a306c544a42e406e01877697
Bochkarev Maxim <qontrol1488@gmail.com>
2017-03-16 16:25:13 +0300

    Added KeyValue data handlers for all library types
    Added KeyValue Hanlders:
    - Abstract (parent class)
    - Json
    - JsonElementValue
    - UrlGet
    - UrlRest
    - XmlAttribute
    - XmlElement

---
+++ SecureData/SecureData/DataHandlers/KeyValueDataHandlers/AbstractKeyValueDataHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace SecureData.DataHandlers.KeyValueDataHandlers
+{
+    public abstract class AbstractKeyValueDataHandler
+    {
+        public string GetSecuredData(string data)
+        {
+            var result = data;
+            foreach (KeyValuePair<string, string> prop in Properties)
+            {
+                result = result.Replace(GetReplaceString(prop.Key, prop.Value), GetReplaceString(prop.Key, GetSecuredValue(prop.Value)));
+            }
+            return result;
+        }
+
+        public Dictionary<string, string> Properties { get; set; }
+
+        public abstract string GetReplaceString(string key, string value);
+
+        public string GetSecuredValue(string value)
+        {
+            return new string('X', value.Length);
+        }
+    }
+}
---
+++ SecureData/SecureData/DataHandlers/KeyValueDataHandlers/UrlGetKeyValueDataHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureData.DataHandlers.KeyValueDataHandlers
+{
+    public class UrlGetKeyValueDataHandler : AbstractKeyValueDataHandler
+    {
+        public override string GetReplaceString(string key, string value)
+        {
+            return (key + "=" + value);
+        }
+    }
+}
