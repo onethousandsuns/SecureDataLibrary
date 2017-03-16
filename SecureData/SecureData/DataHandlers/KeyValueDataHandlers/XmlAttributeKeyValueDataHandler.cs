d600c1c1039be615f90789bed4221bfe5dd15680
Bochkarev Maxim <qontrol1488@gmail.com>
2017-03-16 16:28:39 +0300

    Removed unnecessary Usings

--- SecureData/SecureData/DataHandlers/KeyValueDataHandlers/XmlAttributeKeyValueDataHandler.cs
+++ SecureData/SecureData/DataHandlers/KeyValueDataHandlers/XmlAttributeKeyValueDataHandler.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SecureData.DataHandlers.KeyValueDataHandlers
+﻿namespace SecureData.DataHandlers.KeyValueDataHandlers
 {
     public class XmlAttributeKeyValueDataHandler : AbstractKeyValueDataHandler
     {
